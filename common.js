8add24e31977e1f0c98a63cb0d202ae7b84d330b
sickma1901@gmail.com <2210511101015@live4.utcc.ac.th>
2024-02-08 11:34:12 +0700

    refactor#3 - add modify return msg

--- common.js
+++ common.js
@@ -8,7 +8,6 @@ function validate(pace) {
 		return {"status":0,"message":msg,"hour":0,"min":0};
  }
 }
-
 function paceCal(pace,dist) {
 	var min = dist * pace;
 		var hour = 0;
@@ -16,6 +15,7 @@ function paceCal(pace,dist) {
 			hour = Math.floor(min / 60);          
 			min = min % 60;
 		}
+		return {"min":min,"hour":hour};
 }
 function calculate(p,d){
 		var msg = "";
@@ -28,5 +28,5 @@ function calculate(p,d){
 		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
-		return {"status":1,"message":msg,"hour":hour,"min":min};
+		return {"status":1,"message":msg,"hour":pc.hour,"min":pc.min};
 }
